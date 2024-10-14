82a7767e26da040718451faf1e796c6265b1de53
Bindu Priya <139218094+gorantlabindupriya@users.noreply.github.com>
2024-10-15 00:56:53 +0530

    Create home.jsx

---
+++ home.jsx
@@ -0,0 +1,61 @@
+<div style={{width: '100%', height: '100%', position: 'relative', background: 'linear-gradient(269deg, white 0%, #AFF1F1 100%)'}}>
+    <div style={{width: 1920, height: 720, left: 0, top: 0, position: 'absolute'}}>
+        <div style={{width: 701, height: 894, left: 1219, top: -87, position: 'absolute', background: 'linear-gradient(124deg, #F6F4E4 0%, #ECC2F5 47%, #B8DCDE 100%)', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}></div>
+        <img style={{width: 563.27, height: 537, left: 1357, top: 122, position: 'absolute', boxShadow: '4px 4px 4px rgba(0, 0, 0, 0.25)'}} src="https://via.placeholder.com/563x537" />
+        <div style={{left: 119.54, top: 122, position: 'absolute', color: 'black', fontSize: 100, fontFamily: 'Allura', fontWeight: '400', wordWrap: 'break-word'}}>Hey There... I’m Bindu</div>
+        <div style={{width: 222, height: 75.01, left: 255.06, top: 289.23, position: 'absolute', transform: 'rotate(-8.40deg)', transformOrigin: '0 0', borderRadius: 9999, border: '3px #18C2C2 dotted'}} />
+        <div style={{width: 641.59, left: 119.54, top: 292, position: 'absolute', color: 'black', fontSize: 30, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 48, wordWrap: 'break-word'}}>A passionate UI/UX designer dedicated to crafting impactful user experiences. Skilled in user-centered design principles and proficient in tools like Figma, Adobe XD, and Sketch, I blend creativity with functionality to create intuitive and visually appealing interfaces.</div>
+        <div style={{width: 377.37, height: 99.26, left: 119.54, top: 625, position: 'absolute', background: 'linear-gradient(111deg, #F5FEFE 0%, #9BE5E5 100%)', borderRadius: 15}} />
+        <div style={{left: 188.23, top: 650.63, position: 'absolute', color: 'black', fontSize: 50, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 48, wordWrap: 'break-word'}}>Get Resume</div>
+        <div style={{width: 640.53, height: 340.56, left: 996.79, top: 255.33, position: 'absolute', transform: 'rotate(153.52deg)', transformOrigin: '0 0', border: '4px black dotted'}}></div>
+        <div style={{width: 485.16, height: 239.65, left: 567.27, top: 631.86, position: 'absolute', transform: 'rotate(153.52deg)', transformOrigin: '0 0', border: '4px black dotted'}}></div>
+        <div style={{width: 135.53, height: 281.17, left: 687.98, top: 531.40, position: 'absolute', transform: 'rotate(153.52deg)', transformOrigin: '0 0', opacity: 0.25, border: '4px black dotted'}}></div>
+        <div style={{width: 150.99, height: 87.86, left: 732.21, top: 618.31, position: 'absolute', transform: 'rotate(153.52deg)', transformOrigin: '0 0', border: '4px black dotted'}}></div>
+        <div style={{width: 1280, height: 100, left: 320, top: 0, position: 'absolute'}}>
+            <div style={{width: 267, height: 41, left: 87, top: 30, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Almendra Display', fontWeight: '400', wordWrap: 'break-word'}}>BINDU  PRIYA</div>
+            <div style={{width: 140, height: 44, left: 1273, top: 22, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Almendra SC', fontWeight: '400', wordWrap: 'break-word'}}>contact </div>
+            <div style={{width: 102, height: 44, left: 1156, top: 22, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Almendra SC', fontWeight: '400', wordWrap: 'break-word'}}>work</div>
+            <div style={{width: 146, height: 44, left: 995, top: 22, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Almendra SC', fontWeight: '400', wordWrap: 'break-word'}}>aout me</div>
+            <div style={{width: 103, height: 44, left: 877, top: 22, position: 'absolute', color: 'black', fontSize: 40, fontFamily: 'Almendra SC', fontWeight: '400', wordWrap: 'break-word'}}>Home</div>
+        </div>
+    </div>
+    <div style={{width: 109, height: 109, left: 777.14, top: 1041.37, position: 'absolute', transform: 'rotate(-97.65deg)', transformOrigin: '0 0'}}>
+        <div style={{width: 58.87, height: 79.07, left: 25.07, top: 14.47, position: 'absolute', background: 'black'}}></div>
+    </div>
+    <div style={{width: 109, height: 109, left: 314.03, top: 2037.76, position: 'absolute', transform: 'rotate(121.87deg)', transformOrigin: '0 0'}}>
+        <div style={{width: 58.87, height: 79.07, left: 25.07, top: 14.47, position: 'absolute', background: 'black'}}></div>
+    </div>
+    <div style={{left: 119.91, top: 848, position: 'absolute', color: 'black', fontSize: 100, fontFamily: 'Allura', fontWeight: '400', wordWrap: 'break-word'}}>My Projects</div>
+    <div style={{width: 1677.08, height: 1148.97, left: 1325.72, top: 901.65, position: 'absolute', transform: 'rotate(89.71deg)', transformOrigin: '0 0', border: '4px black dotted'}}></div>
+    <div style={{width: 819.08, height: 418.79, left: 569.32, top: 2005.32, position: 'absolute', background: 'linear-gradient(111deg, #F5FEFE 0%, #9BE5E5 100%)', borderRadius: 15}} />
+    <img style={{width: 510.53, height: 436.37, left: 230.05, top: 1996.53, position: 'absolute'}} src="https://via.placeholder.com/511x436" />
+    <div style={{width: 1249.64, height: 540, left: 338, top: 980.17, position: 'absolute'}}>
+        <div style={{width: 819.08, height: 418.79, left: 430.56, top: 60.61, position: 'absolute'}}>
+            <div style={{width: 819.08, height: 418.79, left: 0, top: 0, position: 'absolute', background: 'linear-gradient(111deg, #F5FEFE 0%, #9BE5E5 100%)', borderRadius: 15}} />
+            <div style={{width: 249.23, height: 249.23, left: 284.92, top: 84.78, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 50, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 88, wordWrap: 'break-word'}}>Recipe Sharing Website</div>
+            <div style={{width: 679.53, left: 92.78, top: 41.39, position: 'absolute', opacity: 0, textAlign: 'center', color: 'black', fontSize: 30, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 48, wordWrap: 'break-word'}}>Website where cooks can share and discover recipes with detailed information and allowing to upload their own recipes, explore new ones, and save favourites. This Website provides kitchen tips, tricks and cooking hacks to improve cooking skills who are eager to learn cooking and also provides meal plans based on user preferences, dietary restrictions. </div>
+        </div>
+        <img style={{width: 640, height: 540, left: 0, top: 0, position: 'absolute'}} src="https://via.placeholder.com/640x540" />
+    </div>
+    <div style={{width: 1249.64, height: 540, left: 338, top: 1440.21, position: 'absolute'}}>
+        <div style={{width: 819.08, height: 418.79, left: 0, top: 73.55, position: 'absolute'}}>
+            <div style={{width: 819.08, height: 418.79, left: 0, top: 0, position: 'absolute', background: 'linear-gradient(111deg, #F5FEFE 0%, #9BE5E5 100%)', borderRadius: 15}} />
+            <div style={{width: 249.23, left: 221.50, top: 121.39, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 50, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 88, wordWrap: 'break-word'}}>Event<br/>Website</div>
+            <div style={{width: 671.04, left: 42.38, top: 41.39, position: 'absolute', opacity: 0, textAlign: 'center', color: 'black', fontSize: 30, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 48, wordWrap: 'break-word'}}>This is an intuitive and user-friendly website that enables the users to both discover and organise events. It provides a detailed info about the event. The platform aims to provide users with seamless tools for finding relevant events based on their interests, while also offering easy-to-use features for creating, managing, and sharing events.</div>
+        </div>
+        <img style={{width: 640, height: 540, left: 1249.64, top: 0, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: '0 0'}} src="https://via.placeholder.com/640x540" />
+    </div>
+    <div style={{width: 249.23, left: 833.02, top: 2082.71, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 50, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 88, wordWrap: 'break-word'}}>Music<br/>Sharing<br/>App</div>
+    <div style={{width: 1440, height: 100, paddingTop: 67, left: 240, top: 4147, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+        <div style={{width: 183, height: 33, textAlign: 'center', color: 'rgba(0, 0, 0, 0.50)', fontSize: 20, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 50, wordWrap: 'break-word'}}>©Bindu Priya Gorantla</div>
+    </div>
+    <div style={{width: 650, height: 282, left: 635, top: 3886, position: 'absolute'}}>
+        <div style={{width: 314, height: 70, left: 168, top: 0, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 48, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 88, wordWrap: 'break-word'}}>Let’s Connect</div>
+        <div style={{width: 650, height: 122, left: 0, top: 90, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 32, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 50, wordWrap: 'break-word'}}>Interested in working together or looking to chat over coffee? Shoot me a message and let’s talk!</div>
+        <div style={{width: 168, height: 50, left: 23, top: 232, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 32, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 50, wordWrap: 'break-word'}}>Get in Touch</div>
+        <div style={{left: 206, top: 232, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 32, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 50, wordWrap: 'break-word'}}>-</div>
+        <div style={{left: 233, top: 232, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 32, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 50, wordWrap: 'break-word'}}>My LinkedIn</div>
+        <div style={{left: 414, top: 232, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 32, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 50, wordWrap: 'break-word'}}>-</div>
+        <div style={{left: 441, top: 232, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 32, fontFamily: 'Amiri', fontWeight: '400', lineHeight: 50, wordWrap: 'break-word'}}>About me</div>
+    </div>
+</div>
